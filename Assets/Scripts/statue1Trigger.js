fa0df251ce0acf7a658453c96a5ff0323204344c
Rebecca Norton <rnorton6@gatech.edu>
2014-04-17 17:37:17 -0400

    scripts for all food/history objects, f and h keys trigger food mode or historic mode

---
+++ Assets/Scripts/statue1Trigger.js
@@ -0,0 +1,24 @@
+﻿//var collisionSound : AudioClip; // set this to your sound in the inspector
+var textDisplay : GUIText; 
+
+
+function Update() {
+	if(Input.GetKeyDown(KeyCode.F)){
+		collider.enabled = false;
+		transform.collider.isTrigger = false;
+	}
+	if(Input.GetKeyDown(KeyCode.H)){
+		collider.enabled = true;
+		transform.collider.isTrigger = true;
+	}
+}
+
+
+function OnTriggerEnter()  {
+	//audio.volume = 1;
+    //audio.PlayOneShot(collisionSound);
+    
+    
+    
+  	textDisplay.text = "statue 1";
+}
