2cda9cc6863b69d17f35ba106f5237d2263df5ad
Rebecca Norton <rnorton6@gatech.edu>
2014-04-17 13:04:29 -0400

    upload

---
+++ Assets/churchTrigger.js
@@ -0,0 +1,11 @@
+﻿//var collisionSound : AudioClip; // set this to your sound in the inspector
+var textDisplay : GUIText; 
+
+ 
+function OnTriggerEnter()  {
+	//audio.volume = 1;
+    //audio.PlayOneShot(collisionSound);
+    
+    
+  	textDisplay.text = "church";
+}
---
+++ Assets/footsteps.js
@@ -0,0 +1,30 @@
+﻿var AudioFile : AudioClip;
+var AudioFile2 : AudioClip;
+function Update() {
+
+
+ 
+	if (Input.GetKeyDown (KeyCode.W) || Input.GetKeyDown (KeyCode.A)
+	|| Input.GetKeyDown (KeyCode.S) || Input.GetKeyDown (KeyCode.D))
+	{
+    	audio.Play();
+	}
+	
+	if (Input.GetKeyUp (KeyCode.W) || Input.GetKeyUp (KeyCode.A)
+	|| Input.GetKeyUp (KeyCode.S) || Input.GetKeyUp (KeyCode.D))
+	{
+    	audio.Stop();
+	}
+	
+	
+}
+
+function OnControllerColliderHit (hit : ControllerColliderHit){
+    if (hit.gameObject.tag == "grass"){
+    	audio.clip = AudioFile;
+      
+    }
+    else if (hit.gameObject.tag == "street"){
+		audio.clip = AudioFile2;
+    }
+}
